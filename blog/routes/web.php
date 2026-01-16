f7fa246d5d98b60815310dcd49dffee339159693
Andreu Verdés <averdes12@gmail.com>
2026-01-16 14:52:11 +0100

    Chapter 74

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+use function Pest\Laravel\get;
 
 Route::get('/', HomeController::class);
 
@@ -40,11 +41,42 @@ Route::resource('posts', PostController::class)
 ->names('posts');
 
 Route::get('/prueba', function () {
-    DB::table('users')
+    /*DB::table('users')
     ->orderBy('id')
     ->chunk(100, function ($users) {
         foreach ($users as $user) {
             echo "Usuario: " . $user->name . "<br>";
         }
-    });
+    });*/
+
+    /*DB::table('users')
+    ->orderBy('id')
+    ->lazy()->each(function ($user) {
+        echo "Usuario: " . $user->name . "<br>";
+    });*/
+
+    //return DB::table('users')->max('id');
+
+    //if(DB::table('users')->where('id', 999)->exists()){
+      //  return "El usuario existe";
+    //} else {
+      //  return "El usuario no existe";
+    //}
+
+    /*return DB::table('users')
+    ->select('id', 'name as Nombre', 'email')
+    ->selectRaw('concat(name, " - ", email) as Info')
+    ->get();*/
+
+    /*return DB::table('posts')
+    ->join('users', 'posts.user_id', '=', 'users.id')
+    ->join('categories', 'posts.category_id', '=', 'categories.id')
+    ->select('posts.*', 'categories.name as category_name', 'users.name as author')
+    ->where('posts.id', '<=', 10)
+    ->get();*/
+
+    return DB::table('users')
+    ->whereNot('email', 'like', '%@example.com')
+    ->get();
+
 });
